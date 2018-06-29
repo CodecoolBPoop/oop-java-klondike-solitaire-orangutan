f2186b4ea584297e9270000c4c413c484f2b3453
Bence Fabian <bence.fabian@codecool.com>
2018-06-29 13:20:10 +0200

    added color
    WARN: not tested

--- src/com/codecool/klondike/Suits.java
+++ src/com/codecool/klondike/Suits.java
@@ -1,19 +1,27 @@
 package com.codecool.klondike;
 
 public enum Suits {
-    HEARTS (1),
-    DIAMONDS (2),
-    SPADES (3),
-    CLUBS (4);
+    HEARTS (1, Color.RED),
+    DIAMONDS (2, Color.RED),
+    SPADES (3, Color.BLACK),
+    CLUBS (4, Color.BLACK);
 
     public final int suitNumber;
+    private final Color color;
 
-    Suits(int suitNumber) {
+    Suits(int suitNumber, Color color) {
         this.suitNumber = suitNumber;
+        this.color = color;
     }
 
     public int getSuitNumber(){
         return this.suitNumber;
     }
+
+    public boolean isSameColor(Suit other) {
+        return color == other.color;
+    }
+
+    private enum Color {RED, BLACK}
 
 }
